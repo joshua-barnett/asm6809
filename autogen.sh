511b0eb3d3cb12b377043c1999d7863ee51e0c2f
Ciaran Anscomb <www@6809.org.uk>
2021-07-30 17:40:55 +0100

    simplify autogen.sh script

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,4 @@ test -n "$srcdir" && cd "$srcdir"
 
 echo "Updating build configuration files, please wait...."
 
-aclocal $ACLOCAL_FLAGS -I m4
-autoheader
-automake --foreign --add-missing -c
-autoconf
+autoreconf -fi
